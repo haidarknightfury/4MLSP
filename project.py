c36bd62b7d903a2c5a6d403aa63ef68f934d8ebe
Haidar <haidar.dargaye@umail.uom.ac.mu>
2020-07-26 13:21:19 +0400

    check missing values

--- project.py
+++ project.py
@@ -7,4 +7,23 @@ from sklearn.metrics import mean_absolute_error, confusion_matrix
 
 # Load the data set
 df = pd.read_csv("titanic.csv")
-print(df.head())
+
+print(df.describe())
+print(df.info())
+
+
+print(df.head(10))
+
+## Missing data
+total = df.isnull().sum().sort_values(ascending=False)
+print(total)
+
+percentageNull = ((df.isnull().sum()/ df.isnull().count()) * 100).sort_values(ascending=False)
+missing_data = pd.concat([total, percentageNull], axis=1, keys=['Total', 'MissingValues %'])
+print(missing_data.head(5))
+
+
+## Drop unecessary variable such as name
+## Y = survived
+## x = Features 
+## No age - replace by average
